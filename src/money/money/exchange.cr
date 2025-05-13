5c19c78e73a7be7d8308917c2c98c5a79a33c243
Sijawusz Pur Rahnama <sija@sija.pl>
2025-05-13 02:42:14 +0200

    Cover edge-cases concerning zero numbers to `Exchange#{exchange_to, with_same_currency}` methods

--- src/money/money/exchange.cr
+++ src/money/money/exchange.cr
@@ -3,8 +3,11 @@ struct Money
     # Exchanges `self` to a new `Money` object in *other_currency*.
     def exchange_to(other_currency : String | Symbol | Currency) : Money
       other_currency = Currency.wrap(other_currency)
-      if currency == other_currency
+      case
+      when other_currency == currency
         self
+      when zero?
+        with_currency(other_currency)
       else
         bank.exchange(self, other_currency)
       end
@@ -12,7 +15,11 @@ struct Money
 
     # Yields *other* `Money` object exchanged to `self.currency`.
     def with_same_currency(other : Money, &)
-      yield other.exchange_to(currency)
+      if other.zero?
+        yield other.with_currency(currency)
+      else
+        yield other.exchange_to(currency)
+      end
     end
   end
 end
