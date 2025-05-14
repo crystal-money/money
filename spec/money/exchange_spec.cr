dde0bf6397d249b2d3fba2b1be1b4595d1148d7e
Sijawusz Pur Rahnama <sija@sija.pl>
2025-05-15 01:53:08 +0200

    Enhance `Money::Exchange` specs

--- spec/money/exchange_spec.cr
+++ spec/money/exchange_spec.cr
@@ -1,11 +1,11 @@
 require "../spec_helper"
 
 describe Money::Exchange do
-  describe "#exchange_to" do
-    bank = Money::Bank::VariableExchange.new(Money::Currency::RateStore::Memory.new)
-    bank.store["EUR", "USD"] = 1.23
-    bank.store["USD", "EUR"] = 3.21
+  bank = Money::Bank::VariableExchange.new(Money::Currency::RateStore::Memory.new)
+  bank.store["EUR", "USD"] = 1.23
+  bank.store["USD", "EUR"] = 3.21
 
+  describe "#exchange_to" do
     it "exchanges the amount properly" do
       with_default_bank(bank) do
         Money.new(100_00, "EUR").exchange_to("USD").should eq Money.new(123_00, "USD")
@@ -13,10 +13,29 @@ describe Money::Exchange do
       end
     end
 
-    it "does no exchange when the currencies are the same" do
+    it "returns self when the currencies are the same" do
       with_default_bank(bank) do
         money = Money.new(1, "USD")
         money.exchange_to("USD").should eq money
+      end
+    end
+
+    it "does not exchange when the amount is zero" do
+      with_default_bank do
+        Money.disallow_currency_conversion!
+
+        Money.zero("USD").exchange_to("EUR")
+          .should eq Money.zero("EUR")
+      end
+    end
+  end
+
+  describe "#with_same_currency" do
+    it "yields the other object exchanged to self.currency" do
+      with_default_bank(bank) do
+        Money.zero("USD").with_same_currency(Money.new(100_00, "EUR")) do |other|
+          other.should eq Money.new(123_00, "USD")
+        end
       end
     end
   end
