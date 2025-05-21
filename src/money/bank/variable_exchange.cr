faaf8f9b8c6efa409d23c2e25b5bce4076560cf1
Sijawusz Pur Rahnama <sija@sija.pl>
2025-05-21 20:13:54 +0200

    Simplify `Bank::VariableExchange#exchange`

--- src/money/bank/variable_exchange.cr
+++ src/money/bank/variable_exchange.cr
@@ -1,16 +1,10 @@
 struct Money
   class Bank::VariableExchange < Bank
     def exchange(from : Money, to : Currency) : Money
-      fractional = calculate_fractional(from, to)
-      fractional *= exchange_rate(from.currency, to)
+      amount =
+        from.amount * exchange_rate(from.currency, to)
 
-      Money.new(fractional: fractional, currency: to, bank: self)
-    end
-
-    private def calculate_fractional(from : Money, to : Currency) : BigDecimal
-      ratio = from.currency.subunit_to_unit.to_big_d / to.subunit_to_unit
-
-      from.fractional / ratio
+      Money.new(amount: amount, currency: to, bank: self)
     end
   end
 end
