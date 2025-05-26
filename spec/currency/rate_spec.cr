2d6216df04b9cb298a664ba7b2aa37708c36fb9b
Sijawusz Pur Rahnama <sija@sija.pl>
2025-05-27 01:30:53 +0200

    Make `Currency::Rate` comparable

--- spec/currency/rate_spec.cr
+++ spec/currency/rate_spec.cr
@@ -23,6 +23,41 @@ describe Money::Currency::Rate do
       JSON
   end
 
+  it "#<=>" do
+    rates = [
+      Money::Currency::Rate.new(
+        Money::Currency.find("USD"),
+        Money::Currency.find("CAD"),
+        1.to_big_d,
+        Time.parse_utc("2025-05-22", "%F"),
+      ),
+      Money::Currency::Rate.new(
+        Money::Currency.find("USD"),
+        Money::Currency.find("CAD"),
+        1.1.to_big_d,
+        Time.parse_utc("2025-05-22", "%F"),
+      ),
+      Money::Currency::Rate.new(
+        Money::Currency.find("USD"),
+        Money::Currency.find("CAD"),
+        1.to_big_d,
+        Time.parse_utc("2025-05-23", "%F"),
+      ),
+      Money::Currency::Rate.new(
+        Money::Currency.find("CAD"),
+        Money::Currency.find("USD"),
+        1.to_big_d,
+        Time.parse_utc("2025-05-22", "%F"),
+      ),
+    ]
+    rates.sort.map(&.to_s).should eq [
+      "USD -> CAD: 1.0",
+      "USD -> CAD: 1.0",
+      "USD -> CAD: 1.1",
+      "CAD -> USD: 1.0",
+    ]
+  end
+
   it "#to_s" do
     rate.to_s.should eq "USD -> CAD: 1.1"
   end
--- src/money/currency/rate.cr
+++ src/money/currency/rate.cr
@@ -1,5 +1,12 @@
 class Money::Currency
   record Rate, from : Currency, to : Currency, value : BigDecimal, updated_at : Time = Time.utc do
+    include Comparable(Rate)
+
+    def <=>(other : Rate) : Int32
+      {from, to, value, updated_at} <=>
+        {other.from, other.to, other.value, other.updated_at}
+    end
+
     def to_s(io : IO) : Nil
       io << from << " -> " << to << ": " << value
     end
