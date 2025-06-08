18312f1edf4b214107a865e0754b1e0d817ea2e2
Sijawusz Pur Rahnama <sija@sija.pl>
2025-06-08 17:28:33 +0200

    Make `Bank` responsible for management of currency rates

--- src/money/bank.cr
+++ src/money/bank.cr
@@ -1,5 +1,5 @@
 struct Money
-  abstract class Bank
+  class Bank
     # An exchange `Currency::RateStore` object, used to persist exchange rate pairs.
     #
     # NOTE: Will return `Money.default_rate_store` if set to `nil` (the default).
@@ -10,23 +10,57 @@ struct Money
       @store || Money.default_rate_store
     end
 
-    def initialize(@store = nil)
+    # An exchange `Currency::RateProvider` object, used to fetch exchange rate pairs.
+    #
+    # NOTE: Will return `Money.default_rate_provider` if set to `nil` (the default).
+    property provider : Currency::RateProvider?
+
+    # :ditto:
+    def provider : Currency::RateProvider
+      @provider || Money.default_rate_provider
     end
 
-    # Returns the exchange rate between `self` and *other* currency, or `nil` if not found.
-    def exchange_rate?(base : Currency, other : Currency) : BigDecimal?
-      base == other ? 1.to_big_d : store[base, other]?
+    def initialize(@store = nil, @provider = nil)
     end
 
-    # Returns the exchange rate between `self` and *other* currency or raises
-    # `UnknownRateError` if not found.
-    def exchange_rate(base : Currency, other : Currency) : BigDecimal
-      base == other ? 1.to_big_d : store[base, other]
+    # Returns an array of supported (registered) currencies.
+    def currencies : Array(Currency)
+      provider.currency_codes.compact_map do |code|
+        Currency.find?(code)
+      end
     end
 
     # Exchanges the given `Money` object to a new `Money` object in
     # *to* `Currency`.
-    abstract def exchange(from : Money, to : Currency) : Money
+    def exchange(from : Money, to : Currency) : Money
+      amount =
+        from.amount * exchange_rate(from.currency, to)
+
+      Money.new(amount: amount, currency: to, bank: self)
+    end
+
+    # Returns the exchange rate between *base* and *other* currency,
+    # or `nil` if not found.
+    def exchange_rate?(base : Currency, other : Currency) : BigDecimal?
+      return 1.to_big_d if base == other
+
+      store[base, other]? ||
+        update_rate(base, other)
+    end
+
+    # Returns the exchange rate between *base* and *other* currency,
+    # or raises `UnknownRateError` if not found.
+    def exchange_rate(base : Currency, other : Currency) : BigDecimal
+      exchange_rate?(base, other) ||
+        raise UnknownRateError.new("No conversion rate known for #{base} -> #{other}")
+    end
+
+    private def update_rate(base : Currency, other : Currency) : BigDecimal?
+      if rate = provider.exchange_rate?(base, other)
+        store << rate
+        rate.value
+      end
+    end
   end
 end
 
--- src/money/bank/variable_exchange.cr
+++ src/money/bank/variable_exchange.cr
@@ -1,10 +1,4 @@
 struct Money
   class Bank::VariableExchange < Bank
-    def exchange(from : Money, to : Currency) : Money
-      amount =
-        from.amount * exchange_rate(from.currency, to)
-
-      Money.new(amount: amount, currency: to, bank: self)
-    end
   end
 end
