8f18d4cede2356647f25c5a28a3acb74d57f27f3
Sijawusz Pur Rahnama <sija@sija.pl>
2025-06-08 19:00:23 +0200

    Add `Currency::RateProvider::Freecurrency`

---
+++ src/money/currency/rate_provider/freecurrency.cr
@@ -0,0 +1,58 @@
+require "log"
+require "http/client"
+
+class Money::Currency
+  # [FreeCurrency](https://freecurrencyapi.com/) currency rate provider.
+  class RateProvider::Freecurrency < RateProvider
+    Log = ::Log.for(self)
+
+    property api_key : String do
+      ENV["FREECURRENCY_API_KEY"]? ||
+        raise "Missing `FREECURRENCY_API_KEY` environment variable"
+    end
+    property host : URI do
+      URI.parse("https://api.freecurrencyapi.com")
+    end
+
+    def initialize(*, @api_key = nil, @host = nil)
+    end
+
+    # https://freecurrencyapi.com/docs/currencies
+    property currency_codes : Array(String) do
+      Log.debug { "Fetching supported currencies" }
+
+      client = HTTP::Client.new(host)
+      client.get("/v1/currencies?apikey=#{api_key}") do |response|
+        unless response.status.ok?
+          raise "Failed to fetch currencies: #{response.status}"
+        end
+
+        result = JSON.parse(response.body_io)
+        currencies =
+          result.as_h["data"].as_h.keys
+
+        currencies
+      end
+    end
+
+    # https://freecurrencyapi.com/docs/latest
+    def exchange_rate?(base : Currency, other : Currency) : Rate?
+      Log.debug { "Updating rate for #{base} -> #{other}" }
+
+      client = HTTP::Client.new(host)
+      client.get(
+        "/v1/latest?apikey=#{api_key}&base_currency=#{base.code}&currencies=#{other.code}"
+      ) do |response|
+        unless response.status.ok?
+          raise "Failed to fetch rates: #{response.status}"
+        end
+
+        result = JSON.parse(response.body_io)
+        rate =
+          result.as_h.dig("data", other.code).to_s.to_big_d
+
+        Rate.new(base, other, rate)
+      end
+    end
+  end
+end
