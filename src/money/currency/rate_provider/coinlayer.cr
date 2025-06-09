258d7ccd86b1e8149de1ae385b5d20297a632f4a
Sijawusz Pur Rahnama <sija@sija.pl>
2025-06-09 20:15:57 +0200

    Add `Currency::RateProvider::Coinlayer`

---
+++ src/money/currency/rate_provider/coinlayer.cr
@@ -0,0 +1,63 @@
+require "log"
+require "http/client"
+
+class Money::Currency
+  # [Coinlayer](https://coinlayer.com/) currency rate provider.
+  class RateProvider::Coinlayer < RateProvider
+    Log = ::Log.for(self)
+
+    property access_key : String do
+      ENV["COINLAYER_ACCESS_KEY"]? ||
+        raise "Missing `COINLAYER_ACCESS_KEY` environment variable"
+    end
+    property host : URI do
+      URI.parse("https://api.coinlayer.com")
+    end
+
+    def initialize(*, @access_key = nil, @host = nil)
+    end
+
+    # https://coinlayer.com/documentation#list
+    getter currency_codes : Array(String) do
+      Log.debug { "Fetching supported currencies" }
+
+      client = HTTP::Client.new(host)
+      client.get("/list?access_key=#{access_key}") do |response|
+        unless response.status.ok?
+          raise "Failed to fetch currencies: #{response.status}"
+        end
+
+        result = JSON.parse(response.body_io)
+        %w[].tap do |currencies|
+          currencies.concat(result.as_h["crypto"].as_h.keys)
+          currencies.concat(result.as_h["fiat"].as_h.keys)
+        end
+      end
+    end
+
+    # https://coinlayer.com/documentation#live
+    def exchange_rate?(base : Currency, other : Currency) : Rate?
+      Log.debug { "Updating rate for #{base} -> #{other}" }
+
+      client = HTTP::Client.new(host)
+      client.get(
+        "/live?access_key=#{access_key}&target=#{other.code}&symbols=#{base.code}"
+      ) do |response|
+        unless response.status.ok?
+          raise "Failed to fetch rates: #{response.status}"
+        end
+
+        result = JSON.parse(response.body_io)
+
+        unless result.as_h["success"].as_bool
+          raise "Rate provider error: #{result.as_h.dig("error", "type")}"
+        end
+
+        rate =
+          result.as_h.dig("rates", base.code).to_s.to_big_d
+
+        Rate.new(base, other, rate)
+      end
+    end
+  end
+end
