9753e5bb4cf7f19ffb1d515c60a589e56ae9ca2e
Sijawusz Pur Rahnama <sija@sija.pl>
2025-07-03 17:51:16 +0200

    Add `Currency::RateProvider::FXRatesAPI`

---
+++ src/money/currency/rate_provider/fx_rates_api.cr
@@ -0,0 +1,60 @@
+require "log"
+require "http/client"
+
+class Money::Currency
+  # [FXRatesAPI](https://fxratesapi.com/) currency rate provider.
+  class RateProvider::FXRatesAPI < RateProvider
+    Log = ::Log.for(self)
+
+    property api_key : String do
+      ENV["FXRATES_API_KEY"]? ||
+        raise "Missing `FXRATES_API_KEY` environment variable"
+    end
+    property host : URI do
+      URI.parse("https://api.fxratesapi.com")
+    end
+
+    def initialize(*, @api_key = nil, @host = nil)
+    end
+
+    # https://fxratesapi.com/docs/endpoints/list-available-currencies
+    getter base_currency_codes : Array(String) do
+      Log.debug { "Fetching supported currencies" }
+
+      client = HTTP::Client.new(host)
+      client.get("/currencies?api_key=#{api_key}") do |response|
+        unless response.status.ok?
+          raise "Failed to fetch currencies: #{response.status}"
+        end
+
+        result = JSON.parse(response.body_io)
+        currencies =
+          result.as_h.keys
+
+        currencies
+      end
+    end
+
+    # https://fxratesapi.com/docs/endpoints/latest-exchange-rates
+    def exchange_rate?(base : Currency, target : Currency) : Rate?
+      Log.debug { "Fetching rate for #{base} -> #{target}" }
+
+      client = HTTP::Client.new(host)
+      client.get("/latest?api_key=#{api_key}&base=#{base.code}&currencies=#{target.code}") do |response|
+        unless response.status.ok?
+          raise "Failed to fetch rates: #{response.status}"
+        end
+
+        result = JSON.parse(response.body_io)
+
+        unless result.as_h["success"].as_bool
+          raise "Rate provider error: #{result.as_h["error"]}"
+        end
+
+        if rate = result.as_h.dig?("rates", target.code)
+          Rate.new(base, target, rate.to_s.to_big_d)
+        end
+      end
+    end
+  end
+end
