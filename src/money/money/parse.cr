718557faf1fb4427ce6e0b09b8ac3eeaaa4ab4ae
Sijawusz Pur Rahnama <sija@sija.pl>
2025-07-06 08:19:15 +0200

    Split `Money::Parse#parse` into smaller methods

--- src/money/money/parse.cr
+++ src/money/money/parse.cr
@@ -22,45 +22,53 @@ struct Money
     end
 
     private def parse(str : String, allow_ambiguous : Bool, &)
-      matched_pattern = PATTERNS.find_value do |pattern|
-        if str =~ pattern
-          {$~["amount"], $~["symbol"], $~["sign"]?}
-        end
-      end
-
-      if matched_pattern
-        amount, symbol, sign = matched_pattern
-      else
-        raise Error.new "Invalid format"
-      end
-
-      currency = begin
-        matches = Currency.select(&.==(symbol))
-        matches = Currency.select(&.symbol.==(symbol)) if matches.empty?
-        matches = Currency.select(&.disambiguate_symbol.==(symbol)) if matches.empty?
-        matches = Currency.select(&.alternate_symbols.try(&.includes?(symbol))) if matches.empty?
-
-        case matches.size
-        when 0
-          raise Error.new "Symbol #{symbol.inspect} didn't matched any currency"
-        when 1
-          matches.first
-        else
-          unless allow_ambiguous
-            raise Error.new "Symbol #{symbol.inspect} matches multiple currencies: #{matches.map(&.to_s)}"
-          end
-          matches.first
-        end
-      end
+      amount, symbol = parse_amount_and_symbol(str)
+      currency = parse_currency(symbol, allow_ambiguous)
 
       if thousands_separator = currency.thousands_separator
         amount = amount.gsub(thousands_separator, '_')
       end
-      amount = "#{sign}#{amount}" if sign
 
       Money.from_amount(amount, currency)
     rescue ex : Money::Error
       yield Error.new "Cannot parse #{str.inspect}", ex
+    end
+
+    private def parse_amount_and_symbol(str : String)
+      matched_pattern =
+        PATTERNS.find_value do |pattern|
+          {$~["amount"], $~["symbol"], $~["sign"]?} if str =~ pattern
+        end ||
+          raise Error.new "Invalid format"
+
+      amount, symbol, sign = matched_pattern
+      amount = "#{sign}#{amount}" if sign
+
+      {amount, symbol}
+    end
+
+    private def parse_currency(symbol : String, allow_ambiguous : Bool) : Currency
+      matches = parse_currencies(symbol)
+
+      case matches.size
+      when 0
+        raise Error.new "Symbol #{symbol.inspect} didn't matched any currency"
+      when 1
+        matches.first
+      else
+        unless allow_ambiguous
+          raise Error.new "Symbol #{symbol.inspect} matches multiple currencies: #{matches.map(&.to_s)}"
+        end
+        matches.first
+      end
+    end
+
+    private def parse_currencies(symbol : String) : Array(Currency)
+      matches = Currency.select(&.==(symbol))
+      matches = Currency.select(&.symbol.==(symbol)) if matches.empty?
+      matches = Currency.select(&.disambiguate_symbol.==(symbol)) if matches.empty?
+      matches = Currency.select(&.alternate_symbols.try(&.includes?(symbol))) if matches.empty?
+      matches
     end
   end
 end
