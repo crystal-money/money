b9d54734b61f767a96eb46f9dc7ce55226277cdc
Sijawusz Pur Rahnama <sija@sija.pl>
2025-07-11 18:39:15 +0200

    Add `Currency::RateProvider::CurrencyLayer`

---
+++ src/money/currency/rate_provider/currency_layer.cr
@@ -0,0 +1,65 @@
+require "log"
+
+class Money::Currency
+  # [CurrencyLayer](https://currencylayer.com/) currency rate provider.
+  class RateProvider::CurrencyLayer < RateProvider
+    include RateProvider::HTTP
+
+    Log = ::Log.for(self)
+
+    property access_key : String do
+      ENV["CURRENCY_LAYER_ACCESS_KEY"]? ||
+        raise "Missing `CURRENCY_LAYER_ACCESS_KEY` environment variable"
+    end
+    property host : URI do
+      URI.parse("https://api.currencylayer.com")
+    end
+
+    def initialize(*, @access_key = nil, @host = nil)
+    end
+
+    # <https://currencylayer.com/documentation#supported_currencies>
+    getter base_currency_codes : Array(String) do
+      Log.debug { "Fetching supported currencies" }
+
+      params = {
+        "access_key": access_key,
+      }
+      request("/list", params) do |response|
+        result = JSON.parse(response.body_io).as_h
+
+        unless result["success"].as_bool
+          raise "Rate provider error: #{result.dig("error", "code")}"
+        end
+
+        currencies =
+          result["currencies"].as_h.keys
+
+        currencies
+      end
+    end
+
+    # <https://currencylayer.com/documentation#real_time_rates>
+    def exchange_rate?(base : Currency, target : Currency) : Rate?
+      Log.debug { "Fetching rate for #{base} -> #{target}" }
+
+      params = {
+        "access_key": access_key,
+        "source":     base.code,
+        "currencies": target.code,
+      }
+      request("/live", params) do |response|
+        result = JSON.parse(response.body_io).as_h
+
+        unless result["success"].as_bool
+          raise "Rate provider error: #{result.dig("error", "code")}"
+        end
+
+        rate =
+          result.dig("quotes", "%s%s" % {base.code, target.code}).to_s.to_big_d
+
+        Rate.new(base, target, rate)
+      end
+    end
+  end
+end
