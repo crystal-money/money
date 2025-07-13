dd3476e5e99b1b42439005a3cc5cde861890bc31
Sijawusz Pur Rahnama <sija@sija.pl>
2025-07-13 20:28:20 +0200

    Add empty initializer to `Currency::RateProvider::Null`

--- src/money/currency/rate_provider/null.cr
+++ src/money/currency/rate_provider/null.cr
@@ -1,6 +1,9 @@
 class Money::Currency
   # Currency rate provider that always returns `nil`.
   class RateProvider::Null < RateProvider
+    def initialize
+    end
+
     getter base_currency_codes : Array(String) do
       [] of String
     end
