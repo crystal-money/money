de9888c15bb5f18781da50734faf8ad6946e62f6
Sijawusz Pur Rahnama <sija@sija.pl>
2025-08-15 23:06:40 +0200

    Extend `Currency::Loader` spec a bit

--- spec/currency/loader_spec.cr
+++ spec/currency/loader_spec.cr
@@ -2,12 +2,20 @@ require "../spec_helper"
 
 describe Money::Currency::Loader do
   context ".load_defaults" do
+    defaults = Money::Currency.load_defaults
+
     it "returns non-empty currency table hash" do
-      Money::Currency.load_defaults.empty?.should be_false
+      defaults.present?.should be_true
+    end
+
+    it "sets the hash key to the currency code" do
+      defaults.each do |key, currency|
+        key.should eq currency.code
+      end
     end
 
     it "returns common currencies within currency table hash" do
-      currencies = Money::Currency.load_defaults.values.map(&.code)
+      currencies = defaults.values.map(&.code)
       currencies.should contain "USD"
       currencies.should contain "EUR"
       currencies.should contain "BTC"
