98924a963141d480d879348abaca7a9884d62f2e
Sijawusz Pur Rahnama <sija@sija.pl>
2025-10-24 15:48:11 +0200

    Refactor `Currency::RateProvider::ECB` with newly added `OneToMany` module

--- src/money/currency/rate_provider/ecb.cr
+++ src/money/currency/rate_provider/ecb.cr
@@ -6,10 +6,13 @@ class Money::Currency
   # [European Central Bank](https://www.ecb.europa.eu).
   class RateProvider::ECB < RateProvider
     include RateProvider::HTTP
+    include RateProvider::OneToMany
 
     Log = ::Log.for(self)
 
-    private BASE_CURRENCY_CODE = "EUR"
+    def base_currency_code : String
+      "EUR"
+    end
 
     getter host : URI do
       URI.parse("https://www.ecb.europa.eu")
@@ -18,32 +21,18 @@ class Money::Currency
     def initialize(*, @host = nil)
     end
 
-    getter base_currency_codes : Array(String) do
-      [BASE_CURRENCY_CODE]
-    end
-
-    getter target_currency_codes : Array(String) do
-      exchange_rates.map(&.target.code)
-    end
-
-    def exchange_rate?(base : Currency, target : Currency) : Rate?
-      exchange_rates.find { |rate| rate.base == base && rate.target == target }
-    end
-
-    protected def exchange_rates : Array(Rate)
+    # <https://www.ecb.europa.eu/stats/policy_and_exchange_rates/euro_reference_exchange_rates/html/index.en.html>
+    protected def exchange_rates : Array(NativeRate)
       Log.debug { "Fetching exchange rates" }
 
       request("/stats/eurofxref/eurofxref-daily.xml") do |response|
-        base =
-          Currency.find(BASE_CURRENCY_CODE)
-
         result = XML.parse(response.body_io)
         rates =
-          result.xpath_nodes("//*[@currency and @rate]").map do |node|
-            Rate.new(base, Currency.find(node["currency"]), node["rate"].to_big_d)
-          end
+          result.xpath_nodes("//*[@currency and @rate]")
 
-        rates
+        rates.map do |node|
+          NativeRate.new(node["currency"], node["rate"].to_big_d)
+        end
       end
     end
   end
