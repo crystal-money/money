b57228f23eb975d1f6b7ab3d28a74869caaa191c
Sijawusz Pur Rahnama <sija@sija.pl>
2025-10-24 22:35:39 +0200

    Add `Currency::RateProvider::NBP`

---
+++ src/money/currency/rate_provider/nbp.cr
@@ -0,0 +1,45 @@
+require "log"
+
+class Money::Currency
+  # Currency rate provider using data sourced from a daily feed of
+  # [Polish National Bank](https://nbp.pl).
+  #
+  # NOTE: Supports only `PLN`-targeted conversions.
+  class RateProvider::NBP < RateProvider
+    include RateProvider::HTTP
+    include RateProvider::ManyToOne
+
+    Log = ::Log.for(self)
+
+    def target_currency_code : String
+      "PLN"
+    end
+
+    getter host : URI do
+      URI.parse("https://api.nbp.pl")
+    end
+
+    def initialize(*, @host = nil)
+    end
+
+    # <https://api.nbp.pl/en.html>
+    protected def exchange_rates : Array(NativeRate)
+      Log.debug { "Fetching exchange rates for #{target_currency_code}" }
+
+      params = {
+        "format": "json",
+      }
+      request("/api/exchangerates/tables/a/", params) do |response|
+        result = JSON.parse(response.body_io).as_a
+        rates = result
+          .find!(&.as_h["table"].==("A"))
+          .as_h["rates"]
+          .as_a
+
+        rates.map do |rate|
+          NativeRate.new(rate["code"].as_s, rate["mid"].to_s.to_big_d)
+        end
+      end
+    end
+  end
+end
