bc37740d248ce9c24f30a67720e0fb9a355f631d
Sijawusz Pur Rahnama <sija@sija.pl>
2025-10-24 23:18:00 +0200

    Add `Currency::RateProvider::CBR`

---
+++ src/money/currency/rate_provider/cbr.cr
@@ -0,0 +1,47 @@
+require "xml"
+require "log"
+
+class Money::Currency
+  # Currency rate provider using data sourced from a daily feed of
+  # [Bank of Russia](https://www.cbr.ru/).
+  #
+  # NOTE: Supports only `RUB`-targeted conversions.
+  class RateProvider::CBR < RateProvider
+    include RateProvider::HTTP
+    include RateProvider::ManyToOne
+
+    Log = ::Log.for(self)
+
+    def target_currency_code : String
+      "RUB"
+    end
+
+    getter host : URI do
+      URI.parse("https://www.cbr.ru")
+    end
+
+    def initialize(*, @host = nil)
+    end
+
+    # <https://www.cbr.ru/development/SXML/>
+    protected def exchange_rates : Array(NativeRate)
+      Log.debug { "Fetching exchange rates for #{target_currency_code}" }
+
+      request("/scripts/XML_daily.asp") do |response|
+        result = XML.parse(response.body_io)
+        rates =
+          result.xpath_nodes("//Valute")
+
+        rates.compact_map do |node|
+          next unless currency_code = node.xpath_string("string(CharCode)").presence
+          next unless rate = node.xpath_string("string(VunitRate)").presence
+
+          NativeRate.new(
+            currency_code,
+            rate.sub(',', '.').to_big_d,
+          )
+        end
+      end
+    end
+  end
+end
